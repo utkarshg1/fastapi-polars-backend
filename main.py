a0bf4f0afd02c0ecda8e1f72c46fca29a5aa6cbe
utkarshg1 <gaikwadujg@gmail.com>
2025-08-09 01:02:08 +0530

    updated code

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 # main.py
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api import upload, summary, aggregation
 
 app = FastAPI(title="EDA App")
 
+allowed_origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:5173").split(",")
+
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite's default port
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
